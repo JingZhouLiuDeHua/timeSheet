36f0f28a5f6f4a1679622ad94d8bcaf448b81887
JingZhouLiuDeHua <yyyttt2016@outlook.com>
2019-09-09 19:54:04 -0400

    add models

--- AddDoctor/models.py
+++ AddDoctor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    First_name = models.CharField(max_length=50)
+    Last_name = models.CharField(max_length=50)
+    FileNumber = models.PositiveIntegerField()
+
--- ReportTable/models.py
+++ ReportTable/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Report(models.Model):
+    Doctor_name = models.CharField(max_length=50)
+    FileNumber = models.PositiveIntegerField()
+    Location = models.CharField(max_length=50)
+    Sector = models.CharField(max_length=50)
+    Work_Date = models.DateTimeField(auto_now_add=True)
+    Time_In = models.DateTimeField(auto_now_add=True)
+    Time_Out = models.DateTimeField(auto_now_add=True)
+    Hours_Worked = models.DateTimeField(auto_now_add=True)
+    Hours_Code = models.CharField(max_length=50)
+    Report_Date = models.DateTimeField(auto_now_add=True)
+    Batch_ID = models.PositiveIntegerField()
+    Company_Code = models.PositiveIntegerField()
