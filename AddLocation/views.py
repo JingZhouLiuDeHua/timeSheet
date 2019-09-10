b74afa8f0e1b3020e8cacc2faa64fd5e4b4e37ca
acapil <43251921+acapil@users.noreply.github.com>
2019-09-10 15:27:30 -0400

    table logic for viewing user

--- AddLocation/views.py
+++ AddLocation/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
+def viewUsers(request):
+    users = User.objects.all()
+    return render(request, 'usertable.html', {'users': users})
+
